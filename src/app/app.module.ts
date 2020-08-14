372f61d16f7c8cf29380a9dcc534b68b825abdd8
Matias Alvarez <69661042+MatiasCL96@users.noreply.github.com>
2020-08-13 23:49:49 -0400

    create-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RecoveryComponent } from './components/recovery/recovery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent
   ],
   imports: [
     BrowserModule,

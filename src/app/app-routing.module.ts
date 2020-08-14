321645d690a4adf52ca5316a8a2f5ad31b751f70
Matias <matias.alvarezve@gmail.com>
2020-08-14 12:20:40 -0400

    routes-login-register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 //Rutas Componentes
+import { LoginComponent } from './components/login/login.component';
 import { RecoveryComponent } from './components/recovery/recovery.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: 'home'    , component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'login'   , component: LoginComponent },
   { path: 'recovery', component: RecoveryComponent },
-  { path: '**', redirectTo: 'registro' }
+  { path: 'home'    , component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
